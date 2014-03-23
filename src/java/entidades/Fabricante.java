d7faf51b7e3ad2d0a428bf631e762eec596178ca
androsoma <androsoma@gmail.com>
2014-03-22 22:16:08 -0500

    Entidad fabricante.

---
+++ src/java/entidades/Fabricante.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author Yeison Osorio
+ */
+@Entity
+public class Fabricante implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @SequenceGenerator(name = "FabricanteSequence", sequenceName = "fabricante_seq", allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FabricanteSequence")
+    private Long id;
+    
+    @Column(length = 50, nullable = false)
+    private String nombre;
+    
+    @Column(length = 50, nullable = true)
+    private String nit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fabricante)) {
+            return false;
+        }
+        Fabricante other = (Fabricante) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+}
