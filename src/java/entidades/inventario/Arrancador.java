a24e74d3deecefcc0304aaf85753bfacf52d0eca
Yeison Osorio <yeison.osorio@interkont.co>
2014-03-28 21:01:44 -0500

    Creación de entidades de Luminaria y subentidades.

---
+++ src/java/entidades/inventario/Arrancador.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidades.inventario;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author Cristian Gutiérrez
+ */
+@Entity
+public class Arrancador implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @SequenceGenerator(name = "ArrancadorSequence", sequenceName = "arrancador_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ArrancadorSequence")
+    private Long id;
+    
+    @Column(length = 255, nullable = true)
+    private String referencia;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_fabricante", nullable = true)
+    private Fabricante fabricante;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_tipoarrancador", nullable = true)
+    private TipoArrancador tipoArrancador;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public TipoArrancador getTipoArrancador() {
+        return tipoArrancador;
+    }
+
+    public void setTipoArrancador(TipoArrancador tipoArrancador) {
+        this.tipoArrancador = tipoArrancador;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Arrancador)) {
+            return false;
+        }
+        Arrancador other = (Arrancador) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.inventario.Arrancador[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/entidades/inventario/Luminaria.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidades.inventario;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author Cristian Gutiérrez
+ */
+@Entity
+public class Luminaria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @SequenceGenerator(name = "LuminariaSequence", sequenceName = "luminaria_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LuminariaSequence")
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_brazoluminaria", nullable = true)
+    private BrazoLuminaria brazoLuminaria;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_balasto", nullable = true)
+    private Balasto balasto;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_arrancador", nullable = true)
+    private Arrancador arrancador;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_tipoherraje", nullable = true)
+    private TipoHerraje tipoHerraje;
+    
+    @ManyToOne
+    @JoinColumn(name = "fk_fabricante", nullable = true)
+    private Fabricante fabricante;
+    
+    @Column(nullable = true)
+    private float altura;
+    
+    @Column(nullable = true)
+    private String nivelIluminacion;
+    
+    @Column(nullable = true)
+    private String potencia;
+    
+    @Column(nullable = true)
+    private String referencia;
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BrazoLuminaria getBrazoLuminaria() {
+        return brazoLuminaria;
+    }
+
+    public void setBrazoLuminaria(BrazoLuminaria brazoLuminaria) {
+        this.brazoLuminaria = brazoLuminaria;
+    }
+
+    public Balasto getBalasto() {
+        return balasto;
+    }
+
+    public void setBalasto(Balasto balasto) {
+        this.balasto = balasto;
+    }
+
+    public Arrancador getArrancador() {
+        return arrancador;
+    }
+
+    public void setArrancador(Arrancador arrancador) {
+        this.arrancador = arrancador;
+    }
+
+    public TipoHerraje getTipoHerraje() {
+        return tipoHerraje;
+    }
+
+    public void setTipoHerraje(TipoHerraje tipoHerraje) {
+        this.tipoHerraje = tipoHerraje;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getNivelIluminacion() {
+        return nivelIluminacion;
+    }
+
+    public void setNivelIluminacion(String nivelIluminacion) {
+        this.nivelIluminacion = nivelIluminacion;
+    }
+
+    public String getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(String potencia) {
+        this.potencia = potencia;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Luminaria)) {
+            return false;
+        }
+        Luminaria other = (Luminaria) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.inventario.Luminaria[ id=" + id + " ]";
+    }
+    
+}
