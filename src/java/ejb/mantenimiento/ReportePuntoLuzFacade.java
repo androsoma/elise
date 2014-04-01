cf62f9e6217558da57cf61129c1f3ff93feed5fe
androsoma <androsoma@gmail.com>
2014-04-01 10:57:01 -0500

    Creacion de Facades para Entidades.

---
+++ src/java/ejb/mantenimiento/ReportePuntoLuzFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejb.mantenimiento;
+
+import entidades.mantenimiento.ReportePuntoLuz;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Stateless
+public class ReportePuntoLuzFacade extends AbstractFacade<ReportePuntoLuz> {
+    @PersistenceContext(unitName = "elisePU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ReportePuntoLuzFacade() {
+        super(ReportePuntoLuz.class);
+    }
+    
+}
